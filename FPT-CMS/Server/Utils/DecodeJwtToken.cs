f6d35cd2e9e1063c68f3772ac97067663185ad4d
hieu4399243 <hieu243268@gmail.com>
2023-07-03 23:48:31 +0700

    Decode

---
+++ FPT-CMS/Server/Utils/DecodeJwtToken.cs
@@ -0,0 +1,20 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+
+namespace Server.Utils;
+
+public class DecodeJwtToken
+{
+    public static string? GetRoleFromToken(string? token)
+    {
+        if (token == null)
+        {
+            return null;
+        }
+        AuthenticationHeaderValue.TryParse(token, out var tokenParse);
+        var handler = new JwtSecurityTokenHandler();
+        var tokenDecode = handler.ReadJwtToken(token);
+        return tokenDecode.Claims.First(p => p.Type.Equals(ClaimTypes.Role)).Value;
+    }
+}
